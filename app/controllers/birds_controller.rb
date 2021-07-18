80bee3a53be252850e6d7721fb3272a3c8bad85a
Sharifdesigns22 <shariftarver@gmail.com>
2021-07-18 10:36:31 -0400

    commit tree 621ddcddaa7c15a75d5723ecbe511c1b5c3d5cd2

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds
   end
-end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+end 
